6767bdad0d25c49d88cd4a002eb21e7906b9d563
Dmm <dmm@mail.ru>
2022-01-12 22:14:29 +0300

    Initial commit

---
+++ core/src/dmmcode/gdx/Panzer.java
@@ -0,0 +1,63 @@
+package dmmcode.gdx;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class Panzer {
+
+    private final Vector2 position = new Vector2();
+    private final Vector2 angle = new Vector2();
+    private final Texture texture;
+    private final TextureRegion textureRegion;
+    private final float size = 64f;
+    private final float halfsize = size / 2;
+    public Panzer(float x, float y) {
+        this(x, y, "panzer_me.png");
+    }
+
+
+    public Panzer(float x, float y, String textureName) {
+        texture = new Texture(textureName);
+        textureRegion = new TextureRegion(texture);
+        position.set(x, y);
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void render(Batch batch) {
+
+        batch.draw(
+                textureRegion,
+                position.x,
+                position.y,
+                halfsize,
+                halfsize,
+                size,
+                size,
+                1,
+                1,
+                angle.angleDeg() - 90
+
+        );
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public void moveTo(Vector2 direction) {
+        position.add(direction);
+
+    }
+
+    public void rotateTo(Vector2 mousePos) {
+
+        angle.set(mousePos).sub(
+                position.x + halfsize,
+                position.y + halfsize);
+    }
+}
